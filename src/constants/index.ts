9f3ecd5f1194c4868e87098427b7f825887755b4
David Christian Jones <david@dcj,codes>
2021-03-17 19:14:50 -0400

    add more items // add accepted keys

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,51 +1,185 @@
 export const alphabet = Object.freeze({
-    a: {
-        label: "Alligator",
-        graphic: "",
-        sound: "",
-        color: "green",
-        text: "white"
-    },
-    b: {
-        label: "Bee",
-        graphic: "",
-        sound: "",
-        color: "yellow",
-        text: "black"
-    },
-    c: {
-        label: "Carrot",
-        graphic: "",
-        sound: "",
-        color: "orange",
-        text: "white"
-    },
-    d: {
-        label: "Dog",
-        graphic: "",
-        sound: "",
-        color: "brown",
-        text: "white"
-    },
-    e: {
-        label: "Elephant",
-        graphic: "",
-        sound: "",
-        color: "#9e9b90",
-        text: "white"
-    },
-    f: {
-        label: "Frog",
-        graphic: "",
-        sound: "",
-        color: "green",
-        text: "white"
-    },
-    g: {
-        label: "Giraffe",
-        graphic: "",
-        sound: "",
-        color: "orange",
-        text: "black"
-    }
-})
+  a: {
+    label: "Alligator",
+    graphic: "",
+    sound: "",
+    color: "green",
+    text: "white",
+  },
+  b: {
+    label: "Bee",
+    graphic: "",
+    sound: "",
+    color: "yellow",
+    text: "black",
+  },
+  c: {
+    label: "Carrot",
+    graphic: "",
+    sound: "",
+    color: "orange",
+    text: "white",
+  },
+  d: {
+    label: "Dog",
+    graphic: "",
+    sound: "",
+    color: "brown",
+    text: "white",
+  },
+  e: {
+    label: "Elephant",
+    graphic: "",
+    sound: "",
+    color: "#9e9b90",
+    text: "white",
+  },
+  f: {
+    label: "Frog",
+    graphic: "",
+    sound: "",
+    color: "green",
+    text: "white",
+  },
+  g: {
+    label: "Giraffe",
+    graphic: "",
+    sound: "",
+    color: "orange",
+    text: "black",
+  },
+  h: {
+    label: "Helicopter",
+    graphic: "",
+    sound: "",
+    color: "red",
+    text: "white",
+  },
+  i: {
+    label: "Iguana",
+    graphic: "",
+    sound: "",
+    color: "green",
+    text: "yrlloe",
+  },
+  j: {
+    label: "Jellyfish",
+    graphic: "",
+    sound: "",
+    color: "blue",
+    text: "white",
+  },
+  k: {
+    label: "Kangaroo",
+    graphic: "",
+    sound: "",
+    color: "#C5C3B0",
+    text: "white",
+  },
+  l: {
+    label: "Lion",
+    graphic: "",
+    sound: "",
+    color: "#DECC9C",
+    text: "brown",
+  },
+  m: {
+    label: "Monkey",
+    graphic: "",
+    sound: "",
+    color: "#85745d",
+    text: "white",
+  },
+  n: {
+    label: "Nose",
+    graphic: "",
+    sound: "",
+    color: "#FAE7DA",
+    text: "black",
+  },
+  o: {
+    label: "Octopus",
+    graphic: "",
+    sound: "",
+    color: "#CE2093",
+    text: "black",
+  },
+  p: {
+    label: "Parrot",
+    graphic: "",
+    sound: "",
+    color: "#6d7a69",
+    text: "white",
+  },
+  q: {
+    label: "Queen",
+    graphic: "",
+    sound: "",
+    color: "#7851a9",
+    text: "white",
+  },
+  r: {
+    label: "Rabbit",
+    graphic: "",
+    sound: "",
+    color: "#9f9289",
+    text: "white",
+  },
+  s: {
+    label: "Shark",
+    graphic: "",
+    sound: "",
+    color: "#34363A",
+    text: "white",
+  },
+  t: {
+    label: "Tomato",
+    graphic: "",
+    sound: "",
+    color: "#ff6347",
+    text: "white",
+  },u: {
+    label: "Umbrella",
+    graphic: "",
+    sound: "",
+    color: "black",
+    text: "white",
+  },
+  v: {
+    label: "Vegetable",
+    graphic: "",
+    sound: "",
+    color: "#FFFDD0",
+    text: "black",
+  },
+  w: {
+    label: "Whale",
+    graphic: "",
+    sound: "",
+    color: "#003042",
+    text: "white",
+  },
+  x: {
+    label: "xylophone",
+    graphic: "",
+    sound: "",
+    color: "yellow",
+    text: "white",
+  },
+  y: {
+    label: "Yak",
+    graphic: "",
+    sound: "",
+    color: "brown",
+    text: "orange",
+  },
+  z: {
+    label: "xylophone",
+    graphic: "",
+    sound: "",
+    color: "pink",
+    text: "white",
+  }
+});
+
+export const acceptedKeys = 'abcdefghijklmnopqrstuvwxyz'.split('')
