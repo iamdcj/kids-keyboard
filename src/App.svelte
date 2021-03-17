44860534b093929a83ab6a190e7a9fc3a9323067
David Christian Jones <david@dcj.codes>
2021-03-16 22:15:28 -0400

    improve

--- src/App.svelte
+++ src/App.svelte
@@ -1,53 +1,16 @@
 <script lang="ts">
+  import Selection from "./components/Selection.svelte";
   import { alphabet } from "./constants";
 
   let selection: string = "";
 
   function handleKeyPress(e) {
-    if(selection === e.key) {
+    if (selection === e.key) {
       return;
     }
-    
+
     selection = e.key;
   }
 </script>
 
-<svelte:window on:keydown={handleKeyPress} />
-<main
-  style="background-color: {alphabet[selection]?.color
-    ? alphabet[selection]?.color
-    : '#fff'}"
->
-  {#if alphabet[selection]?.label}
-    <h1
-      style="color: {alphabet[selection]?.text
-        ? alphabet[selection]?.text
-        : '#fff'}"
-    >
-      {alphabet[selection].label}!
-    </h1>
-  {/if}
-</main>
-
-<style>
-  main {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100vw;
-    height: 100vh;
-    color: #fff;
-    transition: background-color ease 200ms;
-  }
-
-  h1 {
-    font-size: 8em;
-    font-weight: 600;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
-</style>
+<Selection on:keydown={handleKeyPress} selection={alphabet[selection]} />
---
+++ src/components/Selection.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  interface Selection {
+    label: string;
+    color: string;
+    text: string;
+  }
+
+  export let selection: Selection;
+</script>
+
+<svelte:window on:keydown />
+<main style="background-color: {selection?.color}">
+  {#if selection?.label}
+    <h1 style="color: {selection?.text ? selection?.text : '#fff'}">
+      {selection.label}!
+    </h1>
+  {/if}
+</main>
+
+<style>
+  main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    height: 100vh;
+    color: #fff;
+    transition: background-color ease 200ms;
+  }
+
+  h1 {
+    font-size: 8em;
+    font-weight: 600;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
